df378a54d531394de517ead3ae2d26d67b33ac7e
dr10n9 <dr10n9@gmail.com>
2025-06-01 20:15:39 +0300

    Update db creds (public not good)

--- lab8/db.php
+++ lab8/db.php
@@ -1,9 +1,9 @@
 <?php
-$host = "127.0.0.1";
+$host = "sql202.infinityfree.com";
 $port = 3306;
-$username = "username";
-$password = "password";
-$database = "web_lab";
+$username = "if0_39135162";
+$password = "XbbFBKQ8u1vIm";
+$database = "rooms_reservations";
 
 $db = new PDO("mysql:host=$host;port=$port",
                $username,
